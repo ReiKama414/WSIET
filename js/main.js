e30a79a3c6c9348815ee0e78f370f3b4ce3b0ac2
Kama <milu77704@gmail.com>
2022-11-10 13:36:21 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    // Cursors
+    var cursor = document.getElementById("cursor");
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(e) {
+        cursor.style.left = e.clientX + "px";
+        cursor.style.top = e.clientY + "px";
+    });
+    cursor.classList.remove("hover");
+
+    for (var hta = document.querySelectorAll(".ht"), ht = hta.length - 1; ht >= 0; ht--) {
+        hta[ht].addEventListener("mouseover", function () { cursor.classList.add("hover"); }), 
+        hta[ht].addEventListener("mouseout", function () { cursor.classList.remove("hover"); });
+    };
+
+    // Nav
+    $('.navbar-toggler, .btn-cls').click(function () {
+        $('.nav-navbar').toggleClass('show');
+    });
+});
